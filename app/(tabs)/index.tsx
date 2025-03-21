cb904e5d324ebe70ef0bd8c1a6d431842dc03382
tonan <126637328+tonantr@users.noreply.github.com>
2025-03-21 13:40:55 -0400

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,44 +1,81 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { StyleSheet, TextInput, Button, View, Text, Platform, Dimensions } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { BASE_URL } from '../config';
+
 
 const { width: screenWidth } = Dimensions.get('window');
 
 export default function HomeScreen() {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    console.log('Logging in with:', email, password);
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}/`,
+        { username, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+      if (response.status === 200 && response.data.access_token) {
+        setError('');
+        await AsyncStorage.setItem('access_token', response.data.access_token);
+        await AsyncStorage.setItem('role', response.data.role);
+        await AsyncStorage.setItem('username', response.data.username);
+
+        if (response.data.role === 'admin') {
+          console.log('AdminScreen')
+          // navigation.navigate('AdminScreen');  
+        } else if (response.data.role === 'user') {
+          console.log('UserScreen')
+        } else {
+          setError('Invalid role');
+        }
+
+      }
+    }
+    catch (err) {
+      setError('Invalid credentials');
+      console.error(err);
+    }
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+return (
+  <View style={styles.container}>
+    <Text style={styles.title}>Login</Text>
 
-      <View style={styles.formContainer}>
-        <TextInput
-          style={[styles.input, { backgroundColor: 'white' }]}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={[styles.input, { backgroundColor: 'white' }]}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <Button title="Login" onPress={handleLogin} />
-      </View>
-
-      <Text>
-        Need an account? Please contact the administrator.
-      </Text>
+    <View style={styles.formContainer}>
+      <TextInput
+        style={[styles.input, { backgroundColor: 'white' }]}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={[styles.input, { backgroundColor: 'white' }]}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
-  );
+
+    <Text>
+      Need an account? Please contact the administrator.
+    </Text>
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
